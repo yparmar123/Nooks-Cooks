643a01330001a54d4dddf08a9f65bb58249f3561
yparmar2080 <yuvrajparmar2080@gmail.com>
2020-06-28 18:52:31 -0400

    modal pop-ups converted to separate pages

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+require('dotenv').config({path:"./config/keys.env"});
+
 const fakeDB = require('./model/fakeDB');
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -13,7 +18,7 @@ app.get("/", (req, res)=>{
     res.render("home", {
         title: "Home Page",
         products: fakeDB.ProductsDB,
-        hero: "Meals and grocery delivered."
+        hero: "Meals and groceries delivered."
     });
 });
 
@@ -25,6 +30,50 @@ app.get("/MealPackages", (req, res) => {
     });
 });
 
-app.listen(3000, ()=>{
+app.get("/login", (req, res)=>{
+    res.render("login", {
+        title: "Log In Page"
+    });
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup", {
+        title: "Sign Up Page"
+    })
+})
+
+
+app.post("/login", (req, res) => {
+    let emailError = "";
+    let passError = "";
+    if(req.body.email === "")
+    {
+        emailError = "You must enter an email address";
+    }
+
+    if(req.body.password === "")
+    {
+        passError = "You must enter a password";
+    }
+
+    if(emailError !== "" || passError !== "")
+    {
+        res.render(`home`, {
+            title: req.title,
+            products: req.products,
+            packages: req.packages,
+            hero: req.hero,
+            emailError: emailError,
+            passError: passError
+        })
+    }
+    else {
+        res.redirect("/");
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
     console.log("Web Server is up and running!");
 });
